5bdedb9ad33c132eac035101c5d7e1d949d24ba8
Lee0701 <hgl1002@naver.com>
2019-08-07 20:33:01 +0900

    Initial commit.

---
+++ src/main/java/city/kobaya/kobayabot/KobayaBotPlugin.java
@@ -0,0 +1,19 @@
+package city.kobaya.kobayabot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KobayaBotPlugin extends JavaPlugin {
+
+    public static KobayaBotPlugin getInstance() {
+        return getPlugin(KobayaBotPlugin.class);
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
