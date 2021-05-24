eea381b4275193ec8b350e525ff9c59c38f5e3dc
Lee0701 <hgl1002@naver.com>
2021-05-24 17:03:13 +0900

    Reorganize discord command to /minecraft and /discord

--- src/main/java/city/kube/bot/discord/DiscordChatListener.java
+++ src/main/java/city/kube/bot/discord/DiscordChatListener.java
@@ -15,11 +15,10 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DiscordChatListener extends ListenerAdapter {
@@ -45,7 +44,7 @@ public class DiscordChatListener extends ListenerAdapter {
                 .filter(it -> it.isCommandMatches(message.getContentDisplay()))
                 .findFirst()
                 .orElse(null);
-        if (registration != null) {
+        if(registration != null) {
             Player player = registration.getPlayer();
 
             KubeCityPlayer kubeCityPlayer = KubeCityPlayer.of(event.getAuthor().getId());
@@ -103,51 +102,103 @@ public class DiscordChatListener extends ListenerAdapter {
         else return false;
 
         String[] args = new String[tokens.countTokens()];
-        for(int i = 0 ; i < args.length ; i++) args[i] = tokens.nextToken();
+        for(int i = 0; i < args.length; i++) args[i] = tokens.nextToken();
 
         command = command.toLowerCase();
         if(command.startsWith(COMMAND_PREFIX)) command = command.substring(COMMAND_PREFIX.length());
         else return false;
 
-        switch(command) {
-        case "list":
+        if(command.equals("list") || (command.equals("minecraft") && args.length >= 1 && args[0].equals("list"))) {
             List<Player> players = new ArrayList<>(plugin.getServer().getOnlinePlayers());
             String title = String.format(plugin.getMessage("discord-command.player-list", "List of online players (%1$d/%2$d)") + "\n", Bukkit.getOnlinePlayers().size(), Bukkit.getMaxPlayers());
             String content = players.stream().map(Player::getPlayerListName).map(name -> "- " + name).collect(Collectors.joining("\n"));
             bot.sendDiscordMessage(new EmbedMessage(message.getTextChannel(), title, content));
             return true;
-        case "whois":
-            KubeCityPlayer kubeCityPlayer = null;
-            String longArgs = message.getContentDisplay().substring(command.length() + COMMAND_PREFIX.length()).trim();
-            if(longArgs.length() > 0) {
-                List<Member> members = message.getGuild().getMembersByEffectiveName(longArgs, true);
-                if(!members.isEmpty()) {
-                    kubeCityPlayer = KubeCityPlayer.of(members.get(0).getUser().getId());
-                }
+
+        } else if(command.equals("discord")) {
+            if(args.length < 1) {
+                message.getChannel().sendMessage("/discord [whois]").queue();
+                return true;
             }
-            if(kubeCityPlayer != null) {
-                String uuid = kubeCityPlayer.getUuid();
-                String minecraftName = kubeCityPlayer.getNickname();
-                Member member = bot.getGuild().getMemberById(kubeCityPlayer.getDiscordId());
-                if(member != null) {
-                    String discordName = member.getEffectiveName();
-                    message.getChannel().sendMessage(String.format(plugin.getMessage(
-                            "registration.player-info-discord",
-                            "Player info:\n" +
-                                    " - UUID: %1$s\n" +
-                                    " - Minecraft Name: %2$s\n" +
-                                    " - Discord Name: %3$s"
-                    ), uuid, minecraftName, discordName)).queue();
+            if(args[0].equals("whois")) {
+                String content = message.getContentDisplay();
+                String longArgs = content.substring(content.indexOf(args[0]) + args[0].length()).trim();
+                KubeCityPlayer kubeCityPlayer = null;
+                if(longArgs.length() > 0) {
+                    List<Member> members = message.getGuild().getMembersByEffectiveName(longArgs, true);
+                    if(!members.isEmpty()) {
+                        kubeCityPlayer = KubeCityPlayer.of(members.get(0).getUser().getId());
+                    }
+                } else {
+                    message.getChannel().sendMessage("/discord whois [name]").queue();
+                }
+                if(kubeCityPlayer != null) {
+                    String uuid = kubeCityPlayer.getUuid();
+                    String minecraftName = kubeCityPlayer.getNickname();
+                    Member member = bot.getGuild().getMemberById(kubeCityPlayer.getDiscordId());
+                    if(member != null) {
+                        String discordName = member.getEffectiveName();
+                        message.getChannel().sendMessage(String.format(plugin.getMessage(
+                                "registration.player-info-discord",
+                                "Player info:\n" +
+                                        " - UUID: %1$s\n" +
+                                        " - Minecraft Name: %2$s\n" +
+                                        " - Discord Name: %3$s"
+                        ), uuid, minecraftName, discordName)).queue();
+                        return true;
+                    }
+                }
+                message.getChannel().sendMessage(plugin.getMessage(
+                        "registration.player-not-found-discord",
+                        "Player not found.")).queue();
+            }
+            return true;
+
+        } else if(command.equals("minecraft")) {
+            if(args.length < 1) {
+                message.getChannel().sendMessage("/minecraft [whois]").queue();
+                return true;
+            }
+            if(args[0].equals("whois")) {
+                if(args.length < 2) {
+                    message.getChannel().sendMessage("/minecraft whois [nickname]").queue();
                     return true;
                 }
+                KubeCityPlayer kubeCityPlayer = null;
+                OfflinePlayer offlinePlayer;
+                try {
+                    UUID uuid = UUID.fromString(args[1]);
+                    offlinePlayer = Bukkit.getOfflinePlayer(uuid);
+                } catch(IllegalArgumentException ex) {
+                    offlinePlayer = Arrays.stream(Bukkit.getOfflinePlayers()).filter(p -> p.getName().equals(args[1])).findAny().orElse(null);
+                }
+                if(offlinePlayer != null) kubeCityPlayer = KubeCityPlayer.of(offlinePlayer.getUniqueId()).orElse(null);
+                if(kubeCityPlayer != null) {
+                    String uuid = kubeCityPlayer.getUuid();
+                    String minecraftName = kubeCityPlayer.getNickname();
+                    Member member = bot.getGuild().getMemberById(kubeCityPlayer.getDiscordId());
+                    if(member != null) {
+                        String discordName = member.getEffectiveName();
+                        message.getChannel().sendMessage(String.format(plugin.getMessage(
+                                "registration.player-info-discord",
+                                "Player info:\n" +
+                                        " - UUID: %1$s\n" +
+                                        " - Minecraft Name: %2$s\n" +
+                                        " - Discord Name: %3$s"
+                        ), uuid, minecraftName, discordName)).queue();
+                        return true;
+                    }
+                }
+                message.getChannel().sendMessage(plugin.getMessage(
+                        "registration.player-not-found-discord",
+                        "Player not found.")).queue();
             }
-            message.getChannel().sendMessage(plugin.getMessage(
-                    "registration.player-not-found-discord",
-                    "Player not found.")).queue();
             return true;
-        default:
+
+        } else {
             return false;
         }
+
     }
 
 }
