2daaa8a4921723f3d03b30293c0d0500003b88cb
Lee0701 <hgl1002@naver.com>
2026-09-10 12:51:20 +0900

    Separate subcommands as functions

--- src/main/java/kr/kubecity/bot/minecraft/BuildingCommandHandler.java
+++ src/main/java/kr/kubecity/bot/minecraft/BuildingCommandHandler.java
@@ -20,7 +20,6 @@ import java.net.URISyntaxException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class BuildingCommandHandler implements TabExecutor {
 
@@ -30,131 +29,145 @@ public class BuildingCommandHandler implements TabExecutor {
     @Override
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, @NonNull String[] args) {
         if(args.length < 1) {
-            sender.sendMessage("Usage:");
-            sender.sendMessage("/" + label + " [vote|register]");
+            usage(sender, label);
             return true;
         }
+        var subLabel = label + ' ' + args[0];
+        var subArgs = Arrays.copyOfRange(args, 1, args.length);
+        switch(args[0]) {
+            case "vote" -> voteCommand(sender, subLabel, subArgs);
+            case "register" -> registerCommand(sender, subLabel, subArgs);
+            default -> usage(sender, label);
+        }
+        return true;
+    }
+
+    private void usage(CommandSender sender, String label) {
+        sender.sendMessage("Usage:");
+        sender.sendMessage("/" + label + " [vote|register]");
+    }
+
+    private void voteCommand(CommandSender sender, String label, String[] args) {
         KubeCityBotPlugin plugin = KubeCityBotPlugin.getInstance();
-        if(args[0].equals("vote")) {
-            if(!(sender instanceof Player player)) {
-                sender.sendMessage(plugin.getMessage(
-                        "missing-permission",
-                        "You must be a player to use this command."
-                ));
-                return true;
-            }
-            var votes = plugin.getFeature(BuildingVotes.class).orElse(null);
-            if(votes == null) {
-                sender.sendMessage(plugin.getMessage(
-                        "building-votes.not-enabled",
-                        "Building votes feature ins not enabled in config."
-                ));
-                return true;
-            }
 
-            if(!player.hasPermission("kubecitybot.building.vote")) {
-                sender.sendMessage(plugin.getMessage(
-                        "missing-permission",
-                        "You don't have permission to use this command."
-                ));
-            }
-
-            if(args.length == 2) {
-                int buildingId = -1;
-                try {
-                    buildingId = Integer.parseInt(args[1]);
-                } catch (NumberFormatException e) {}
-                var building = Building.BUILDINGS.get(buildingId);
-                if(building == null) {
-                    player.sendMessage(String.format(plugin.getMessage(
-                            "building-votes.no-such-building",
-                            "No such building with id %1$d"
-                    ), buildingId));
-                    return true;
-                }
-                String uuid = player.getUniqueId().toString();
-                if(building.getBuilderUuid().equals(uuid)) {
-                    player.sendMessage(plugin.getMessage(
-                            "building-votes.no-self-voting",
-                            "You cannot vote to your building."
-                    ));
-                    return true;
-                }
-                votes.getDatabase().putVote(new Vote(building.getWikiPageId(), uuid, new Date()));
-                player.sendMessage("Successfully voted to " + building.getName());
-                building.spawnHologram();
-
-            } else {
-                var buildings = Building.BUILDINGS.values().stream().filter(building -> building.getHologram().isViewer(player));
-                var lines = new ArrayList<>(buildings.map(building -> {
-                    TextComponent message = new TextComponent(
-                            String.format(plugin.getMessage("building-votes.building-list-item", "- %1$s\n"), building.getName()));
-                    message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/building vote %d", building.getWikiPageId())));
-                    return message;
-                }).toList());
-                if(lines.isEmpty()) {
-                    lines.add(new TextComponent(
-                            plugin.getMessage("building-votes.building-list-empty", "There are no buildings nearby.") + "\n"));
-                }
-                lines.addFirst(new TextComponent(
-                        plugin.getMessage("building-votes.building-list-header", "Buildings nearby:") + "\n"));
-                player.spigot().sendMessage(lines.toArray(new TextComponent[0]));
-
-            }
-            return true;
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage(plugin.getMessage(
+                    "not-player",
+                    "You must be a player to use this command."
+            ));
+            return;
         }
-        if(args[0].equals("register")) {
-            if(!(sender instanceof Player player)) {
-                sender.sendMessage(plugin.getMessage(
-                        "missing-permission",
-                        "You must be a player to use this command."
-                ));
-                return true;
-            }
+        var votes = plugin.getFeature(BuildingVotes.class).orElse(null);
+        if(votes == null) {
+            sender.sendMessage(plugin.getMessage(
+                    "building-votes.not-enabled",
+                    "Building votes feature ins not enabled in config."
+            ));
+            return;
+        }
 
-            if(!player.hasPermission("kubecitybot.building.register")) {
-                sender.sendMessage(plugin.getMessage(
-                        "missing-permission",
-                        "You don't have permission to use this command."
-                ));
-            }
+        if(!player.hasPermission("kubecitybot.building.vote")) {
+            sender.sendMessage(plugin.getMessage(
+                    "missing-permission",
+                    "You don't have permission to use this command."
+            ));
+        }
 
-            if(args.length < 2) {
-                sender.sendMessage(plugin.getMessage(
-                        "missing-building-name",
-                        "Usage: /building register <name>"
-                ));
-                return true;
-            }
-
-            TextComponent message = new TextComponent(
-                    plugin.getMessage("building-votes.click-to-register", "Click on this text to register a building on this location."));
-            String url = plugin.getMessage("building-votes.wiki-url", "https://example.com/");
-            String preload = plugin.getMessage("building-votes.register-preload", "");
-            String name = Arrays.stream(args).skip(1).collect(Collectors.joining(" "));
-            Location location = player.getLocation();
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        if(args.length == 1) {
+            int buildingId = -1;
             try {
-                URI uri = new URIBuilder(url)
-                        .setPath("/wiki/" + name)
-                        .setParameter("action", "edit")
-                        .setParameter("preload", preload)
-                        .addParameter("preloadparams[]", Integer.toString((int) location.getX()))
-                        .addParameter("preloadparams[]", Integer.toString((int) location.getY()))
-                        .addParameter("preloadparams[]", Integer.toString((int) location.getZ()))
-                        .addParameter("preloadparams[]", location.getWorld().getName())
-                        .addParameter("preloadparams[]", player.getUniqueId().toString())
-                        .addParameter("preloadparams[]", player.getName())
-                        .addParameter("preloadparams[]", dateFormat.format(new Date()))
-                        .build();
-                message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, uri.toASCIIString()));
-                player.spigot().sendMessage(message);
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
+                buildingId = Integer.parseInt(args[0]);
+            } catch (NumberFormatException _) {}
+            var building = Building.BUILDINGS.get(buildingId);
+            if(building == null) {
+                player.sendMessage(String.format(plugin.getMessage(
+                        "building-votes.no-such-building",
+                        "No such building with id %1$d"
+                ), buildingId));
+                return;
             }
-            return true;
+            String uuid = player.getUniqueId().toString();
+            if(building.getBuilderUuid().equals(uuid)) {
+                player.sendMessage(plugin.getMessage(
+                        "building-votes.no-self-voting",
+                        "You cannot vote to your building."
+                ));
+                return;
+            }
+            votes.getDatabase().putVote(new Vote(building.getWikiPageId(), uuid, new Date()));
+            player.sendMessage("Successfully voted to " + building.getName());
+            building.spawnHologram();
+
+        } else {
+            var buildings = Building.BUILDINGS.values().stream().filter(building -> building.getHologram().isViewer(player));
+            var lines = new ArrayList<>(buildings.map(building -> {
+                TextComponent message = new TextComponent(
+                        String.format(plugin.getMessage("building-votes.building-list-item", "- %1$s\n"), building.getName()));
+                message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/%s %d", label, building.getWikiPageId())));
+                return message;
+            }).toList());
+            if(lines.isEmpty()) {
+                lines.add(new TextComponent(
+                        plugin.getMessage("building-votes.building-list-empty", "There are no buildings nearby.") + "\n"));
+            }
+            lines.addFirst(new TextComponent(
+                    plugin.getMessage("building-votes.building-list-header", "Buildings nearby:") + "\n"));
+            player.spigot().sendMessage(lines.toArray(new TextComponent[0]));
+
         }
-        return false;
+    }
+
+    private void registerCommand(CommandSender sender, String label, String[] args) {
+        KubeCityBotPlugin plugin = KubeCityBotPlugin.getInstance();
+
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage(plugin.getMessage(
+                    "not-player",
+                    "You must be a player to use this command."
+            ));
+            return;
+        }
+
+        if(!player.hasPermission("kubecitybot.building.register")) {
+            sender.sendMessage(plugin.getMessage(
+                    "missing-permission",
+                    "You don't have permission to use this command."
+            ));
+        }
+
+        if(args.length < 1) {
+            sender.sendMessage(plugin.getMessage(
+                    "missing-building-name",
+                    "Usage: /" + label + " <name>"
+            ));
+            return;
+        }
+
+        TextComponent message = new TextComponent(
+                plugin.getMessage("building-votes.click-to-register", "Click on this text to register a building on this location."));
+        String url = plugin.getMessage("building-votes.wiki-url", "https://example.com/");
+        String preload = plugin.getMessage("building-votes.register-preload", "");
+        String name = String.join(" ", args);
+        Location location = player.getLocation();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            URI uri = new URIBuilder(url)
+                    .setPath("/wiki/" + name)
+                    .setParameter("action", "edit")
+                    .setParameter("preload", preload)
+                    .addParameter("preloadparams[]", Integer.toString((int) location.getX()))
+                    .addParameter("preloadparams[]", Integer.toString((int) location.getY()))
+                    .addParameter("preloadparams[]", Integer.toString((int) location.getZ()))
+                    .addParameter("preloadparams[]", location.getWorld().getName())
+                    .addParameter("preloadparams[]", player.getUniqueId().toString())
+                    .addParameter("preloadparams[]", player.getName())
+                    .addParameter("preloadparams[]", dateFormat.format(new Date()))
+                    .build();
+            message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, uri.toASCIIString()));
+            player.spigot().sendMessage(message);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- src/main/java/kr/kubecity/bot/minecraft/DiscordCommandHandler.java
+++ src/main/java/kr/kubecity/bot/minecraft/DiscordCommandHandler.java
@@ -8,6 +8,7 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -25,113 +26,157 @@ public class DiscordCommandHandler implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length < 1) {
-            sender.sendMessage("Usage:");
-            sender.sendMessage("/" + label + " [register|unregister|whois]");
+            usage(sender, label);
             return true;
         }
-        if(args[0].equals("whois")) {
-            KubeCityPlayer kubeCityPlayer = null;
-
-            if(args.length >= 2) {
-                OfflinePlayer offlinePlayer;
-                try {
-                    UUID uuid = UUID.fromString(args[1]);
-                    offlinePlayer = Bukkit.getOfflinePlayer(uuid);
-                } catch(IllegalArgumentException ex) {
-                    offlinePlayer = Arrays.stream(Bukkit.getOfflinePlayers()).filter(p -> p.getName().equals(args[1])).findAny().orElse(null);
-                }
-                if(offlinePlayer != null) kubeCityPlayer = KubeCityPlayer.of(offlinePlayer.getUniqueId()).orElse(null);
-            } else if(sender instanceof Player) {
-                kubeCityPlayer = KubeCityPlayer.of((Player) sender).orElse(null);
-            }
-
-            if(kubeCityPlayer != null) {
-                String uuid = kubeCityPlayer.getUuid();
-                String minecraftName = kubeCityPlayer.getNickname();
-                String discordName = KubeCityBotPlugin.getInstance().getBot().getJda()
-                        .getGuildById(KubeCityBotPlugin.getInstance().getServerId())
-                        .getMemberById(kubeCityPlayer.getDiscordId()).getEffectiveName();
-                sender.sendMessage(String.format(KubeCityBotPlugin.getInstance().getMessage(
-                        "registration.player-info",
-                        ChatColor.GREEN + "Player info:\n" + ChatColor.WHITE +
-                                " - UUID: %1$s\n" +
-                                " - Minecraft Name: %2$s\n" +
-                                " - Discord Name: %3$s"
-                ), uuid, minecraftName, discordName));
-            } else {
-                sender.sendMessage(KubeCityBotPlugin.getInstance().getMessage(
-                        "registration.player-not-found",
-                        ChatColor.YELLOW + "Player not found."));
-            }
-            return true;
+        var subLabel = label + ' ' + args[0];
+        var subArgs = Arrays.copyOfRange(args, 1, args.length);
+        switch(args[0]) {
+            case "whois" -> whoisCommand(sender, subLabel, subArgs);
+            case "register" -> registerCommand(sender, subLabel, subArgs);
+            case "unregister" -> unregisterCommand(sender, subLabel, subArgs);
+            default -> usage(sender, label);
         }
-        if(args[0].equals("register")) {
-            if(sender instanceof Player) {
-                Player player = (Player) sender;
-                KubeCityPlayer kubeCityPlayer = KubeCityPlayer.of(player).orElse(null);
-                if(kubeCityPlayer == null || !kubeCityPlayer.isLinked()) {
-                    Registration registration = new Registration(player);
-                    KubeCityPlayer.REGISTRATIONS.add(registration);
+        return true;
+    }
 
-                    String registerCommand = "/register " + registration.getKey();
-                    String url = "http:register/" + registration.getKey();
-                    TextComponent register = new TextComponent(registerCommand);
-                    register.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                            new BaseComponent[] {new TextComponent(KubeCityBotPlugin.getInstance().getMessage(
-                                    "registration.alt-command-info", "or click to copy an alternative command"))}
-                    ));
-                    register.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
-                    register.setColor(ChatColor.AQUA.asBungee());
-                    String[] msg = String.format(KubeCityBotPlugin.getInstance().getMessage(
-                            "registration.type-in-discord",
-                            "Type \"%1$s\" in Discord chat to complete."), "<COMMAND>").split("<COMMAND>");
-                    TextComponent root = new TextComponent(
-                            new TextComponent(msg[0]), register, new TextComponent(msg.length > 1 ?msg[1] : ""));
-                    sender.spigot().sendMessage(root);
-                } else {
-                    sender.sendMessage(KubeCityBotPlugin.getInstance().getMessage(
-                            "registration.already-registered",
-                            ChatColor.YELLOW + "You are already registered! use /discord unregister to unregister first."
-                    ));
-                }
-            }
-            return true;
+    private void usage(CommandSender sender, String label) {
+        sender.sendMessage("Usage:");
+        sender.sendMessage("/" + label + " [register|unregister|whois]");
+    }
+
+    private void whoisCommand(CommandSender sender, String label, String[] args) {
+        KubeCityPlayer kubeCityPlayer = null;
+
+        if(args.length >= 1) {
+            OfflinePlayer offlinePlayer = findOfflinePlayer(args[0]);
+            if(offlinePlayer != null) kubeCityPlayer = KubeCityPlayer.of(offlinePlayer.getUniqueId()).orElse(null);
+        } else if(sender instanceof Player) {
+            kubeCityPlayer = KubeCityPlayer.of((Player) sender).orElse(null);
         }
-        if(args[0].equals("unregister")) {
-            if(args.length >= 2) {
-                if(sender.hasPermission("kubecitybot.admin")) {
-                    KubeCityPlayer kubeCityPlayer = KubeCityPlayer.of(KubeCityBotPlugin.getInstance().getServer().getPlayer(args[1])).orElse(null);
-                    if(kubeCityPlayer != null) {
-                        KubeCityPlayer.PLAYER_MAP.remove(kubeCityPlayer.getDiscordId());
-                        sender.sendMessage(String.format(KubeCityBotPlugin.getInstance().getMessage(
-                                "registration.unregistered-player", ChatColor.GREEN + "Unregistered player %1$s!"
-                        ), kubeCityPlayer.getNickname()));
-                    } else {
-                        sender.sendMessage(String.format(KubeCityBotPlugin.getInstance().getMessage(
-                                "registration.player-is-not-registered", ChatColor.YELLOW + "Player %1$s is not registered!"
-                        ), args[1]));
-                    }
-                    return true;
-                }
-            }
-            if(sender instanceof Player) {
-                Player player = (Player) sender;
-                KubeCityPlayer kubeCityPlayer = KubeCityPlayer.of(player).orElse(null);
-                if(kubeCityPlayer != null) {
-                    KubeCityPlayer.PLAYER_MAP.remove(kubeCityPlayer.getDiscordId());
-                    sender.sendMessage(KubeCityBotPlugin.getInstance().getMessage(
-                            "registration.unregister-complete", ChatColor.GREEN + "You are now unregistered."));
 
-                    KubeCityBotPlugin.getInstance().getFeature(GroupLinker.class).ifPresent(linker -> linker.reloadPlayer(player));
-
-                } else {
-                    sender.sendMessage(KubeCityBotPlugin.getInstance().getMessage(
-                            "registration.already-unregistered", ChatColor.YELLOW + "You are already unregistered!"));
-                }
-                return true;
-            }
+        if(kubeCityPlayer != null) {
+            String uuid = kubeCityPlayer.getUuid();
+            String minecraftName = kubeCityPlayer.getNickname();
+            String discordName = KubeCityBotPlugin.getInstance().getBot().getGuild()
+                    .getMemberById(kubeCityPlayer.getDiscordId()).getEffectiveName();
+            sender.sendMessage(String.format(KubeCityBotPlugin.getInstance().getMessage(
+                    "registration.player-info",
+                    ChatColor.GREEN + "Player info:\n" + ChatColor.WHITE +
+                            " - UUID: %1$s\n" +
+                            " - Minecraft Name: %2$s\n" +
+                            " - Discord Name: %3$s"
+            ), uuid, minecraftName, discordName));
+        } else {
+            sender.sendMessage(KubeCityBotPlugin.getInstance().getMessage(
+                    "registration.player-not-found",
+                    ChatColor.YELLOW + "Player not found."));
         }
-        return false;
+    }
+
+    private void registerCommand(CommandSender sender, String label, String[] args) {
+        KubeCityBotPlugin plugin = KubeCityBotPlugin.getInstance();
+
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage(plugin.getMessage(
+                    "not-player",
+                    "You must be a player to use this command."
+            ));
+            return;
+        }
+
+        KubeCityPlayer kubeCityPlayer = KubeCityPlayer.of(player).orElse(null);
+        if(kubeCityPlayer == null || !kubeCityPlayer.isLinked()) {
+            Registration registration = new Registration(player);
+            KubeCityPlayer.REGISTRATIONS.add(registration);
+
+            String registerCommand = "/register " + registration.getKey();
+            String url = "http:register/" + registration.getKey();
+            TextComponent register = new TextComponent(registerCommand);
+            register.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(plugin.getMessage(
+                    "registration.alt-command-info", "or click to copy an alternative command"))));
+            register.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
+            register.setColor(ChatColor.AQUA.asBungee());
+            String[] msg = String.format(plugin.getMessage(
+                    "registration.type-in-discord",
+                    "Type \"%1$s\" in Discord chat to complete."), "<COMMAND>").split("<COMMAND>");
+            TextComponent root = new TextComponent(
+                    new TextComponent(msg[0]), register, new TextComponent(msg.length > 1 ?msg[1] : ""));
+            sender.spigot().sendMessage(root);
+        } else {
+            sender.sendMessage(plugin.getMessage(
+                    "registration.already-registered",
+                    ChatColor.YELLOW + "You are already registered! use /discord unregister to unregister first."
+            ));
+        }
+    }
+
+    private void unregisterCommand(CommandSender sender, String label, String[] args) {
+        KubeCityBotPlugin plugin = KubeCityBotPlugin.getInstance();
+
+        if(args.length >= 1) {
+            if(!sender.hasPermission("kubecitybot.admin")) {
+                sender.sendMessage(plugin.getMessage(
+                        "missing-permission",
+                        "You must be a player to use this command."
+                ));
+                return;
+            }
+            OfflinePlayer offlinePlayer = findOfflinePlayer(args[0]);
+            if(offlinePlayer == null) {
+                sender.sendMessage(String.format(plugin.getMessage(
+                        "registration.player-is-not-registered", ChatColor.YELLOW + "Player %1$s is not registered!"
+                ), args[0]));
+                return;
+            }
+            KubeCityPlayer kubeCityPlayer = KubeCityPlayer.of(offlinePlayer.getUniqueId()).orElse(null);
+            if(kubeCityPlayer == null) {
+                sender.sendMessage(String.format(plugin.getMessage(
+                        "registration.player-is-not-registered", ChatColor.YELLOW + "Player %1$s is not registered!"
+                ), offlinePlayer.getName()));
+                return;
+            }
+            KubeCityPlayer.PLAYER_MAP.remove(kubeCityPlayer.getDiscordId());
+            sender.sendMessage(String.format(plugin.getMessage(
+                    "registration.unregistered-player", ChatColor.GREEN + "Unregistered player %1$s!"
+            ), kubeCityPlayer.getNickname()));
+            return;
+        }
+
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage(plugin.getMessage(
+                    "not-player",
+                    "You must be a player to use this command."
+            ));
+            return;
+        }
+
+        KubeCityPlayer kubeCityPlayer = KubeCityPlayer.of(player).orElse(null);
+        if(kubeCityPlayer != null) {
+            KubeCityPlayer.PLAYER_MAP.remove(kubeCityPlayer.getDiscordId());
+            sender.sendMessage(plugin.getMessage(
+                    "registration.unregister-complete", ChatColor.GREEN + "You are now unregistered."));
+
+            plugin.getFeature(GroupLinker.class).ifPresent(linker -> linker.reloadPlayer(player));
+
+        } else {
+            sender.sendMessage(plugin.getMessage(
+                    "registration.already-unregistered", ChatColor.YELLOW + "You are already unregistered!"));
+        }
+    }
+
+    private  OfflinePlayer findOfflinePlayer(String arg) {
+        OfflinePlayer offlinePlayer;
+        try {
+            UUID uuid = UUID.fromString(arg);
+            offlinePlayer = Bukkit.getOfflinePlayer(uuid);
+        } catch(IllegalArgumentException ex) {
+            offlinePlayer = Arrays.stream(Bukkit.getOfflinePlayers())
+                    .filter(p -> Objects.equals(p.getName(), arg))
+                    .findAny()
+                    .orElse(null);
+        }
+        return offlinePlayer;
     }
 
     @Override
